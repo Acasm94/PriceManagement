822b68c719bef71ea471be1bfe9ee795e8275607
Acasm94 <acasm94@gmail.com>
2017-12-10 21:24:56 +0100

    First commit

---
+++ PriceManagementService/src/main/java/com/sep/pricemanagement/PriceManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sep.pricemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceManagementServiceApplication.class, args);
+	}
+}
